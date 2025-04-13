466ce78e55102ddda431eb92a9376b0eca4c53f3
i-chaeyeon <leechae0605@naver.com>
2025-04-13 23:06:09 +0900

    chore: fix cred path

--- configs/config_firebase.py
+++ configs/config_firebase.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("assets/firebase-service-account.json")
+cred = credentials.Certificate("firebase-key.json")
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
