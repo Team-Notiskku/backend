2bd263a9a6bc6b412339ea64ae9214fc331e303d
i-chaeyeon <leechae0605@naver.com>
2025-04-15 15:54:20 +0900

    chore: set credentials for actions

--- configs/config_firebase.py
+++ configs/config_firebase.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("assets/firebase-key.json")
+# cred = credentials.Certificate("assets/firebase-key.json")
+cred = credentials.Certificate("firebase-key.json")
 
 firebase_admin.initialize_app(cred)
 
