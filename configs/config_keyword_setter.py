fa70e579dd060903eb2e80b853dce4967e9d06cd
i-chaeyeon <leechae0605@naver.com>
2025-04-27 21:31:20 +0900

    feat: Implement Outdated Keyword Notice Crawler

---
+++ configs/config_keyword_setter.py
@@ -0,0 +1,15 @@
+KEYWORD_BASE_URLS = {
+    "기숙사" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EA%B8%B0%EC%88%99%EC%82%AC",
+    "등록금" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EB%93%B1%EB%A1%9D%EA%B8%88",
+    "수강신청" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EC%88%98%EA%B0%95%EC%8B%A0%EC%B2%AD",
+    "공모전" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EA%B3%B5%EB%AA%A8%EC%A0%84",
+    "장학금" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EC%9E%A5%ED%95%99%EA%B8%88",
+    "복학" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EB%B3%B5%ED%95%99",
+    "성적" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EC%84%B1%EC%A0%81",
+    "휴학" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%ED%9C%B4%ED%95%99",
+    "졸업" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EC%A1%B8%EC%97%85",
+    "봉사" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EB%B4%89%EC%82%AC",
+    "해외" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%ED%95%B4%EC%99%B8",
+    "인턴" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EC%9D%B8%ED%84%B4",
+    "계절" : "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&srCategoryId1=&srSearchKey=article_title&srSearchVal=%EA%B3%84%EC%A0%88"
+}
---
+++ setter/keyword_setter.py
@@ -0,0 +1,73 @@
+from playwright.sync_api import sync_playwright
+from configs.config_keyword_setter import KEYWORD_BASE_URLS
+from crawler.modules import generate_hash
+from urllib.parse import urljoin
+from google.cloud import firestore
+from configs.config_firebase import db
+
+def set_notice(keyword):
+    base_url = KEYWORD_BASE_URLS.get(keyword)
+    xpaths = {
+    "title": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dt/a',
+    "category": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dt/span[1]',
+    "uploader": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dd/ul/li[2]',
+    "date": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dd/ul/li[3]',
+    "views": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dd/ul/li[4]/span',
+    "link": '//*[@id="jwxe_main_content"]/div/div/div[1]/div[1]/ul/li[{}]/dl/dt/a'
+    }
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        
+        for page_num in range(3):
+            offset = page_num * 10
+            notice_url = f"?mode=list&&articleLimit=10&article.offset={offset}"
+            full_url = urljoin(base_url, notice_url)
+
+            page.goto(full_url)
+            page.wait_for_load_state("load")
+
+            notice_count = 0
+            i = 1
+            per_page = 10
+
+            while notice_count < per_page:
+                try:
+                    title = page.locator(xpaths["title"].format(i)).inner_text(timeout=1000)
+                    category = page.locator(xpaths["category"].format(i)).inner_text(timeout=1000)
+                    uploader = page.locator(xpaths["uploader"].format(i)).inner_text(timeout=1000)
+                    date = page.locator(xpaths["date"].format(i)).inner_text(timeout=1000)
+                    views = page.locator(xpaths["views"].format(i)).inner_text(timeout=1000)
+                    link = urljoin(base_url, page.locator(xpaths["link"].format(i)).get_attribute("href"))
+
+
+                    hash = generate_hash("전체", None, None, title, uploader)
+
+                    notice_data = {
+                        "type": "전체",
+                        "title": title,
+                        "category": category,
+                        "uploader": uploader,
+                        "date": date,
+                        "views": views,
+                        "url": link,
+                        "updated_at": firestore.SERVER_TIMESTAMP,
+                    }
+                    db.collection("notices").document(hash).set(notice_data, merge=True)
+                    notice_count += 1
+                    i += 1
+
+                except Exception as e:
+                    print("오류")
+                    browser.close()
+                    return
+
+        browser.close()
+
+    print("완료")
+
+
+for keyword in KEYWORD_BASE_URLS.keys():
+    set_notice(keyword)
+    print(keyword)
